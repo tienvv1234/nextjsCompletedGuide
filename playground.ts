81b343ca684eee75d29a9bfa80ee8e35c6f2791a
tienvv1234 <tienvv1234@gmail.com>
2022-11-16 17:26:41 +0700

    single from array types

--- playground.ts
+++ playground.ts
@@ -1,41 +1,12 @@
 interface Person {
     name: string;
 }
+type SingleType<T> = T extends any[] ? T[number] : T;
 
-interface Student extends Person {
-    age: number;
-}
-
-interface PostGraduateStudent extends Student {
-    projects: string[];
-}
-
-type StudentInfo<T extends any = Student> = T extends Student ?  {
-    data: T;
-    grades: number[];
-} : string
-
-type Car = { engine: string; }
 export default function play() {
-    
-    function logStudentInfo(info: StudentInfo) {
-        console.log(info.data.name);
-        console.log(info.data.age);
-    }
+    type Type1 = SingleType<string[]>;
+    type Type2 = SingleType<number[]>;
+    type Type3 = SingleType<Person>;
 
-    function logStudentInfo1(info: StudentInfo<Car>) {
-        console.log(info);
-    }
-
-    const info = {
-        data: {
-            name: 'John',
-            age: 20,
-            projects: ['Project 1', 'Project 2']
-        },
-        grades: [1, 2, 3]
-    }
-
-    logStudentInfo(info);
-    logStudentInfo1('Hello');
+    type Type4 = string[][number];
 }
