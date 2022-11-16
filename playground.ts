dcd4be8770120f7b92a42284059c7b1a0625b9f6
tienvv1234 <tienvv1234@gmail.com>
2022-11-16 16:50:27 +0700

    generic extend default type

--- playground.ts
+++ playground.ts
@@ -8,7 +8,15 @@ interface Car {
     name: string;
 }
 
-class Logger<T extends Person> {
+type CarOne = {
+    brand: string;
+    age: number;
+    city: string;
+    name: string;
+}
+
+// this is default value when we don't pass any value
+class Logger<T extends Person = CarOne> {
     log(items: Array<T>, callback: (i: T) => void) {
         items.forEach((item) => {
             callback(item);
@@ -30,8 +38,9 @@ class Teacher implements Person {
 }
 
 export default function play() {
-    const loggerPerson = new Logger<Teacher>();
-    const persons = [{ name: 'John', age: 0, city: '' }, { name: 'Jane', age: 0, city: ''}];
+    // default value is car one
+    const loggerPerson = new Logger();
+    const persons = [{ name: 'John', age: 0, city: '', brand: '' }, { name: 'Jane', age: 0, city: '', brand: '' }];
     loggerPerson.log(persons, (person: Person) => {
         console.log(person.name);
     });
