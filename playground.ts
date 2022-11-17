84622d09fa2e6757ac972a768b20847472fd3c78
tienvv1234 <tienvv1234@gmail.com>
2022-11-17 11:58:35 +0700

    inter keyword

--- playground.ts
+++ playground.ts
@@ -2,12 +2,18 @@ interface Person {
     name: string;
 }
 
+// this expression T extends () => string
+// this mean T is a function that return string
+// type ReturnType<T> = T extends () => string ? string : number 
+
+// this expression
+// inter keyword will create new (maybe return type or type) type R 
+type ReturnType<T> = T extends () => infer R ? R : number;
+
+
 export default function play() {
-    function logger(...args: any) {
+    function logger() {
         return "hello world";
     }
-
-    const kindaLogger: typeof logger = (name: string, age: number) => "hello world";
-
-    kindaLogger('abc', 20)
+    const loggerReturn1: ReturnType<typeof logger> = "hi";
 }
