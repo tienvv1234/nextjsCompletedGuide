12096824a7c781814e213ef527cfc8db61673dd6
tienvv1234 <tienvv1234@gmail.com>
2022-11-17 14:51:22 +0700

    multi generic type

--- playground.ts
+++ playground.ts
@@ -3,9 +3,15 @@ interface Person {
     age: number;
 }
 
-type PersonKeys = keyof Person; // 'name' | 'age'
+type Logger<
+T = string, 
+E = number, 
+R = string
+> = (param1: T, param2: E) => R;
+
 
 export default function play() {
-    const personKey: PersonKeys = 'age';
-    console.log(personKey);
+    const superLogger: Logger<string, number, string> = (param1, param2) => {
+        return 'param1 + param2';
+    }
 }
