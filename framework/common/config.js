cda3e55336337ee65385e3f993a43bf9922eb8ff
tienvv1234 <tienvv1234@gmail.com>
2022-11-22 08:55:49 +0700

    re-write the tsconfig file with corresponding framework

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs')
 const merge = require('deepmerge')
 
 function withFramewordConfig(defaultConfig = {}) {
@@ -7,9 +8,15 @@ function withFramewordConfig(defaultConfig = {}) {
     // '../shopify/next.config'
     console.log(path.join('../', framework, 'next.config'))
     const frameworkNextConfig = require(path.join('../', framework, 'next.config'));
-    console.log(frameworkNextConfig)
+    console.log('frameworkNextConfig', frameworkNextConfig)
     const config = merge(defaultConfig, frameworkNextConfig);
-    console.log(config)
+    console.log('config', config)
+    const tsPath = path.join(process.cwd(), 'tsconfig.json')
+    const tsConfig = require(tsPath);
+    tsConfig.compilerOptions.paths['@framework'] = [`framework/${framework}`];
+    tsConfig.compilerOptions.paths['@framework/*'] = [`framework/${framework}/*`];
+    // save file with new config ts
+    fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2))
     return config;
 }
 
