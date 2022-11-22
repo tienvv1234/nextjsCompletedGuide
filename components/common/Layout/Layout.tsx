8f8163ce729f1e853132ef560af6a6f928a0e5e0
tienvv1234 <tienvv1234@gmail.com>
2022-11-22 17:06:01 +0700

    tailwind variable

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 import { PropsWithChildren } from 'react';
-
+import s from './Layout.module.css';
 // react 18 children no longer to FC, it belong to PropsWithChildren
 const Layout = ({ children }: PropsWithChildren) => {
     return (
-        <div className="layout">
-            {children}
+        <div className={s.root}>
+            <main 
+            style={{color: "var(--primary)"}}
+            className="fit">
+                {children}
+            </main>
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { withFrameworkConfig } = require('./framework/common/config')
 
 module.exports = withFrameworkConfig({
     framework: {
-        name: "shopify"
+        name: 'shopify'
     },
     i18n: {
         locales: ['en-US', 'es'],
