880d0c3a596322b3a03568faf4f34231c7091188
tienvv1234 <tienvv1234@gmail.com>
2022-11-22 15:10:25 +0700

    config styles and Noop component

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 // react 18 children no longer to FC, it belong to PropsWithChildren
-const Layout = ({ children } : PropsWithChildren) => {
+const Layout = ({ children }: PropsWithChildren) => {
     return (
         <div className="layout">
             {children}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
+import "@assets/main.css";
 import { AppProps } from 'next/app';
 import { ElementType, PropsWithChildren } from 'react';
+
+const Noop = ({children}: PropsWithChildren) => <>{children}</>;
 
 // update type & { Layout: ElementType } to { Layout: ElementType } & PropsWithChildren
 // function MyApp({Component, pageProps}: AppProps & {Component: { Layout: ElementType } & PropsWithChildren}) {
 function MyApp({Component, pageProps}: AppProps & {Component: { Layout: ElementType<PropsWithChildren> }}) {    
-    const Layout = Component.Layout;
+    const Layout = Component.Layout ?? Noop;
 
     return (
         <Layout>
