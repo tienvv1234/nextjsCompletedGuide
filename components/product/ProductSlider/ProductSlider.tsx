ddfeb67b4edad49aede036013dac42e445b7c2b6
tienvv1234 <tienvv1234@gmail.com>
2022-12-01 16:09:07 +0700

    implement keen slider react

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,11 +1,57 @@
-import { FC, PropsWithChildren } from 'react';
+import {
+    FC,
+    PropsWithChildren,
+    Children,
+    isValidElement,
+    cloneElement,
+    useState,
+} from 'react';
 import s from './ProductSlider.module.css';
-
-const ProductSlider: FC<PropsWithChildren> = ({children}) => {
+import { useKeenSlider } from 'keen-slider/react';
+import cn from 'classnames';
+const ProductSlider: FC<PropsWithChildren> = ({ children }) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+        initial: 0,
+        loop: true,
+        slideChanged(s) {
+            setCurrentSlide(s.track.details.rel);
+        }
+    });
     return (
         <div className={s.root}>
-            <div className='keen-slider h-full transition-opacity duration-150'>
-                {children}
+            <div
+                ref={sliderRef}
+                className='keen-slider h-full transition-opacity duration-150'
+            >
+                <button
+                    onClick={() => slider.current?.prev()}
+                    className={cn(s.leftControl, s.control)}
+                />
+                <button
+                    onClick={() => slider.current?.next()}
+                    className={cn(s.rightControl, s.control)}
+                />
+                {Children.map(children, (child) => {
+                    if (isValidElement(child)) {
+                        return {
+                            ...child,
+                            props: {
+                                ...child.props,
+                                className: `${
+                                    child.props.className
+                                        ? `${child.props.className}`
+                                        : ''
+                                } keen-slider__slide`,
+                            },
+                        };
+                        // return cloneElement(child, {
+                        //     className: `${child.props.className ? `${child.props.className}` : ''} keen-slider__slide`,
+                        // });
+                        // the same
+                    }
+                    return child;
+                })}
             </div>
         </div>
     );
